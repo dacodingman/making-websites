6d01ac031a20072b1f0538815e5124cd08bdedb1
dacodingman <74630512+dacodingman@users.noreply.github.com>
2021-03-05 13:34:32 -0500

    Update froggy.js

--- froggy.js
+++ froggy.js
@@ -18,27 +18,6 @@ function camofrogAppear() {
 
 }
 
-
-function updateTime() {
-
-    var currentTime = new Date()
-    var hours = currentTime.getHours()
-    var minutes = currentTime.getMinutes()
-    if (minutes < 10) {
-        minutes = "0" + minutes
-    }
-
-    var timeString = hours + ":" + minutes + " ";
-    if ("hours > 11") {
-
-        timeString += "AM"
-    } else {
-        timeString += "PM";
-    }
-    document.getElementById("clock").innerHTML = timeString;
-}
-setInterval(updateTime, 1000);
-
 function getAge() {
 
     var x
